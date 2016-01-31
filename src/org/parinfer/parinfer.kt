c68c7285d1eea07434ba0b1c570f378862e225eb
Chris Oakman <chris@oakmac.com>
2016-01-31 01:45:49 -0600

    saving state

--- src/org/parinfer/parinfer.kt
+++ src/org/parinfer/parinfer.kt
@@ -5,6 +5,9 @@
 
 package org.parinfer
 
+import java.lang.Math
+import java.util.ArrayList
+
 //--------------------------------------------------------------------------------------------------
 // Constants / Predicates
 //--------------------------------------------------------------------------------------------------
@@ -42,10 +45,10 @@ fun isCloseParen(c: String) : Boolean {
 // Result Structure
 //--------------------------------------------------------------------------------------------------
 
-final class StackItm(lineNo: Int, x: Int, ch: Char, indentDelta: Int) {
+final class StackItm(lineNo: Int, x: Int, ch: String, indentDelta: Int) {
     public val lineNo: Int = lineNo
     public val x: Int = x
-    public val ch: Char = ch
+    public val ch: String = ch
     public val indentDelta: Int = indentDelta
 }
 
@@ -55,17 +58,17 @@ final class Result(text: String, mode: String) {
     public val origText: String = text
     //public var origLines: String = ...
 
-    public var lines: Array<String> = arrayOf<String>()
+    public var lines: ArrayList<String> = arrayListOf<String>()
     public var lineNo: Int = -1
     public var ch: String = ""
     public var x: Int = 0
 
-    public var parenStack: Array<StackItm> = arrayOf<StackItm>()
+    public var parenStack: ArrayList<StackItm> = arrayListOf<StackItm>()
 
     public var parenTrailLineNo: Int? = null
     public var parenTrailStartX: Int? = null
     public var parenTrailendX: Int? = null
-    public var parenTrailOpeners: Array<StackItm> = arrayOf<StackItm>()
+    public var parenTrailOpeners: ArrayList<StackItm> = arrayListOf<StackItm>()
 
     public var cursorX: Int? = null
     public var cursorLine: Int? = null
@@ -110,29 +113,101 @@ val errorMessages = hashMapOf(
     ERROR_UNCLOSED_QUOTE to "String is missing a closing quote.",
     ERROR_UNCLOSED_PAREN to "Unmatched open-paren.")
 
+// TODO: finish the error functions
+
 //--------------------------------------------------------------------------------------------------
 // String Operations
 //--------------------------------------------------------------------------------------------------
 
-// TODO: write me
+fun insertWithinString(orig: String, idx: Int, insert: String) : String {
+    return orig.substring(0, idx) + insert + orig.substring(idx)
+}
+
+fun replaceWithinString(orig: String, start: Int, end: Int, replace: String) : String {
+    return orig.substring(0, start) + replace + orig.substring(end)
+}
+
+fun removeWithinString(orig: String, start: Int, end: Int) : String {
+    return orig.substring(0, start) + orig.substring(end)
+}
+
+fun repeatString(text: String, n: Int) : String {
+    return text.repeat(n)
+}
+
+fun getLineEnding(text: String) : String {
+    // TODO: write me
+    return "\n"
+}
 
 //--------------------------------------------------------------------------------------------------
-// Line Operations
+// Line operations
 //--------------------------------------------------------------------------------------------------
 
-// TODO: write me
+fun insertWithinLine(result: Result, lineNo: Int, idx: Int, insert: String) {
+    val line = result.lines[lineNo]
+    result.lines[lineNo] = insertWithinString(line, idx, insert)
+}
+
+fun replaceWithinLine(result: Result, lineNo: Int, start: Int, end: Int, replace: String) {
+    val line = result.lines[lineNo]
+    result.lines[lineNo] = replaceWithinString(line, start, end, replace)
+}
+
+fun removeWithinLine(result: Result, lineNo: Int, start: Int, end: Int) {
+    val line = result.lines[lineNo]
+    result.lines[lineNo] = removeWithinString(line, start, end)
+}
+
+fun initLine(result: Result, line: String) {
+    result.x = 0
+    result.lineNo = result.lineNo + 1
+    result.lines.add(line)
+
+    // reset line-specific state
+    result.commentX = null
+    result.indentDelta = 0
+}
+
+// if the current character has changed, commit its change to the current line.
+fun commitChar(result: Result, origCh: String) {
+    val ch = result.ch
+    if (origCh != ch) {
+        replaceWithinLine(result, result.lineNo, result.x, result.x + origCh.length, ch)
+    }
+    result.x = result.x + ch.length
+}
 
 //--------------------------------------------------------------------------------------------------
 // Misc Util
 //--------------------------------------------------------------------------------------------------
 
-// TODO: write me
+fun clamp(valN: Int, minN: Int?, maxN: Int?) : Int {
+    var returnN = valN
+    if (minN != null) {
+        returnN = Math.max(minN, valN)
+    }
+    if (maxN != null) {
+        returnN = Math.min(maxN, valN)
+    }
+    return returnN
+}
+
+// NOTE: the parinfer.js "peek" function is not really possible in a statically typed language
 
 //--------------------------------------------------------------------------------------------------
 // Character functions
 //--------------------------------------------------------------------------------------------------
 
-// TODO: write me
+fun isValidCloseParen(parenStack: ArrayList<StackItm>, ch: String) : Boolean {
+    val parenStackSize = parenStack.size()
+    if (parenStackSize == 0) {
+        return false
+    }
+
+    val lastStackItm = parenStack.get(parenStackSize - 1)
+    return lastStackItm.ch == PARENS.get(ch)
+}
 
 //--------------------------------------------------------------------------------------------------
 // Cursor functions
@@ -162,5 +237,6 @@ val errorMessages = hashMapOf(
 // Public API
 //--------------------------------------------------------------------------------------------------
 
-fun main(args: Array<String>) = //println("hello parinfer!")
-    println( PARENS.get("{") )
+fun main(args: Array<String>) {
+    println( repeatString("YaY", 4) )
+}
