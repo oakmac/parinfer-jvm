f0916c58fc74ecbc0114976fb8dd680f896a5983
Chris Oakman <chris@oakmac.com>
2016-02-01 14:11:26 -0600

    update project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
 (defproject parinfer-test "0.1.0"
+
   :description "The test harness for parinfer-jvm"
+  :url "https://github.com/oakmac/parinfer-jvm"
+
+  :license {:name "ISC License"
+            :url "https://github.com/oakmac/parinfer-jvm/blob/master/LICENSE.md"
+            :distribution "repo"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]]
+
   :resource-paths ["lib/parinfer.jar"]
   :source-paths ["src-clj"]
   :main parinfer-test.core)
-
-;; TODO: add url, license
